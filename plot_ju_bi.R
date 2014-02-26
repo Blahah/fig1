31527793adf04ba51e0c1e2139d5f4c3ea7e5ea2
Richard Smith <rds45@cam.ac.uk>
2014-02-26 16:47:47 +0000

    prettier plot

--- plot_ju_bi.R
+++ plot_ju_bi.R
@@ -51,40 +51,62 @@ d2[d2[,4] == "CentralAsia",5] = "black"
 d[d[,4] == "Oceania",5] = "turquoise"
 d2[d2[,4] == "Oceania",5] = "turquoise"
 
-pdf("ju_bi.pdf")
-par(mfrow = c(2,1), mar = c(2,4,4,2))
+# let's play with ggplot and plyr
+library(ggplot2)
+library(plyr)
 
-plot(d[,2], axes = F, xlab = "", ylab = expression(paste("ALDER amplitude (x", 10^-4, ")")), pch = 20, col = d[,5], ylim = c(0, 4e-4), cex = 0.7)
-for (i in 1:nrow(d)){
-	lines( c(i, i), c(d[i,2]-d[i,3], d[i,2]+d[i,3]), col = d[i,5])
+# get the data ready
+fixdf <- function(df) {
+  # sensible name
+  names(df)[4] <- "Continent"
+  # sort the continents by median
+  cms <- ddply(df, .(Continent), summarise, med=median(Amp))
+  cms <- cms[with(cms, order(med)),]
+  df$Continent <- factor(df$Continent,
+                         levels=cms$Continent,
+                         ordered=TRUE)
+  return(df)
 }
 
-axis(2, at = c(0, 2e-4, 4e-4), lab = c("0", "2", "4"))
-mtext("A. Ju|'hoan_North", adj = 0)
-arrows(50, 0, 20, 0, length = 0.1)
-text(51, 0, lab = "low", adj = 0)
-text(19, 0, lab = "high", adj = 1)
+d <- fixdf(d)
+d2 <- fixdf(d2)
 
-
-legend("topright", legend = c("Africa", "Europe", "Middle East", "Central Asia", "East Asia", "Oceania", "Americas"), col = c("red", "orange", "blue", "black", "green", "turquoise", "yellow"), pch = 20, bty = "n", cex = 0.7)
-
-for (i in 1:3){
-	text(1, 3e-4 - i*0.4e-4, lab = paste(i, ".", d[i,1]), col = d[i,5], adj = 0, cex = 0.6)
+# our plotting functions
+# we'll go with a pointrange plot faceted by continent
+pointrangeplot <- function(df, title) {
+  p <- ggplot(df, aes(x=reorder(Population, Amp), y=Amp, ymin=Amp-SE, ymax=Amp+SE, colour=Continent)) +
+    geom_pointrange() +
+    ylab("ALDER amplitiude") +
+    theme_bw() +
+    guides(colour=FALSE) +
+    facet_grid(.~Continent, scales="free_x", space="free_x", labeller=labelfunc) +
+    theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5),
+          plot.title = element_text(hjust=0, size=35)) +
+    xlab("") +
+    ggtitle(title)
+  print(p)
+  return(p)
 }
 
 
-plot(d2[,2], axes = F, xlab = "", ylab =  expression(paste("ALDER amplitude (x", 10^-4, ")")), pch = 20, col = d2[,5], cex = 0.7, ylim = c(0, 0.00013))
-for (i in 1:nrow(d2)){
-        lines( c(i, i), c(d2[i,2]-d2[i,3], d2[i,2]+d2[i,3]), col = d2[i,5])
+# we need to fix the facet titles - some get cut off
+labelfunc <- function(variable, value) {
+  v <- sapply(value, function(x) {
+    if (x == "Oceania") {
+      return("Oce-\nania")
+    } else if (x == "MiddleEast") {
+      return("Middle\nEast")
+    } else if (x == "EastAsia") {
+      return("East Asia")
+    } else {
+      return(levels(x)[x])
+    }
+  })
+  return(v)
 }
-axis(2, at = c(0, 0.00006, 0.00012), lab = c("0", "0.6", "1.2"))
-mtext("B. Biaka", adj = 0)
 
-for (i in 1:3){
-        text(1, 0.00008 - i*0.13e-4, lab = paste(i, ".", d2[i,1]), col = d2[i,5], adj = 0, cex = 0.6)
-}
-arrows(50, 0, 20, 0, length = 0.1)
-text(51, 0, lab = "low", adj = 0)
-text(19, 0, lab = "high", adj = 1)
-
-dev.off()
+# arrange the plots one above the other
+library(gridExtra)
+p1 <- pointrangeplot(d, "A")
+p2 <- pointrangeplot(d2, "B")
+grid.arrange(p1, p2, ncol=1, nrow=2)
